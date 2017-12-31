8c0e8c7cfd6aac4d6cca6d08e4e51e5e26b5a37a
Unknown <gersoncalienes@gmail.com>
2017-12-31 17:53:13 -0500

    Added sssets and redesign of site

---
+++ scripts/hamburger.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$(".cross").hide();
+	$(".menu-mobile").hide();
+	$(".hamburger").click(function() {
+		$(".menu-mobile").slideToggle("slow", function() {
+			$(".hamburger").hide();
+			$(".cross").show();
+		});
+	});
+
+	$(".cross").click(function() {
+		$(".menu-mobile").slideToggle("slow", function() {
+			$(".cross").hide();
+			$(".hamburger").show();
+		});
+	});
+});
