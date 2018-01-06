9dcee1d2152076de67a975a5b6b0855b0096f4c7
Unknown <gersoncalienes@gmail.com>
2018-01-06 18:30:41 -0500

    Hamburger menu fixed on mobile. will close on selection now.

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
-	$(".cross").hide();
-	$(".menu-mobile").hide();
-	$(".hamburger").click(function() {
-		$(".menu-mobile").slideToggle("slow", function() {
-			$(".hamburger").hide();
-			$(".cross").show();
-		});
-	});
+  $('.cross').hide();
+  $('.menu-mobile').hide();
+  $('.hamburger').click(function() {
+    $('.menu-mobile').slideToggle('slow', function() {
+      $('.hamburger').hide();
+      $('.cross').show();
+    });
+  });
 
-	$(".cross").click(function() {
-		$(".menu-mobile").slideToggle("slow", function() {
-			$(".cross").hide();
-			$(".hamburger").show();
-		});
-	});
+  $('.cross').click(function() {
+    $('.menu-mobile').slideToggle('slow', function() {
+      $('.cross').hide();
+      $('.hamburger').show();
+    });
+  });
+
+  $('.menu-mobile-list-items').click(function() {
+    $('.menu-mobile').slideToggle('slow', function() {
+      $('.cross').hide();
+      $('.hamburger').show();
+    });
+  });
 });
