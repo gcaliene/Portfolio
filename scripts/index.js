7106851137678cd4601662654afae08e589b48b8
Windows <gersoncalienes@gmail.com>
2018-01-13 00:34:30 -0500

    Updated media queries

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
   const currentYear = new Date().getFullYear();
-  $('#footer').append(`<p>&copy;  1989-${currentYear}.</p>`);
+  $('#footer').append(`<p>&copy; ${currentYear}</p>`);
 });
