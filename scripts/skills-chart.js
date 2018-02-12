766ffafbc10072af5f01dcd97685d8d427eb2967
Windows <gersoncalienes@gmail.com>
2018-02-12 13:02:00 -0500

    Finished graph. justa couple more checks and will merge.

--- scripts/skills-chart.js
+++ scripts/skills-chart.js
@@ -2,13 +2,21 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
   type: 'polarArea',
   data: {
-    labels: ['Node', 'React', 'Vanilla JS', 'jQuery', 'HTML5/CSS3'],
+    labels: [
+      'Node',
+      'React',
+      'JS & jQuery',
+      'MongoDB',
+      'HTML5/CSS3',
+      'Photoshop',
+      'Python'
+    ],
     datasets: [
       {
         label: '# of Votes',
-        data: [4, 3, 4, 3, 4],
+        data: [4, 4, 5, 4, 5, 3, 2],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, .2)',
           'rgba(54, 162, 235, 0.2)',
           'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
@@ -31,31 +39,68 @@ var myChart = new Chart(ctx, {
     title: {
       display: true,
       position: 'top',
-      text: 'Skills by yours truly.'
+      text: '*Skills are always improving.'
     },
     scale: {
       ticks: {
-        max: 4,
-        min: 0,
+        max: 5,
+        min: 1,
         stepSize: 1,
         beginAtZero: true,
         callback: function(value, index, values) {
+          // console.log(value);
           // return '$' + value;
           switch (value) {
             case 1:
-              return 'Just Learning...';
+              return 'Some';
               break;
             case 2:
-              return 'Novice';
+              return 'Still Learning';
               break;
             case 3:
-              return 'Average';
+              return 'Working Knowledge';
               break;
             case 4:
-              return 'Advanced';
+              return 'Great';
               break;
             case 5:
-              return 'Master';
+              return 'Awesome!';
+              break;
+            default:
+              console.log('Graph Loaded');
+          }
+        }
+      }
+    },
+    tooltips: {
+      callbacks: {
+        label: function(tooltipItem, chartData) {
+          // console.log(chartData.labels[tooltipItem.index]);
+          // console.log(tooltipItem.yLabel);
+          // console.log(chartData);
+          switch (tooltipItem.yLabel) {
+            case 1:
+              return 'Some';
+              break;
+            case 2:
+              return `I'm still learning ${
+                chartData.labels[tooltipItem.index]
+              }.`;
+              break;
+            case 3:
+              return `I have a working knowledge of ${
+                chartData.labels[tooltipItem.index]
+              }`;
+              break;
+            case 4:
+              return `My skills in ${
+                chartData.labels[tooltipItem.index]
+              } are great!`;
+              break;
+            case 5:
+              return `My skills in ${
+                chartData.labels[tooltipItem.index]
+              } are AWESOME!`;
               break;
             default:
               console.log('Graph Loaded');
